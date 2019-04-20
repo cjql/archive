710c24019d37bbd45161034bdd601fea6fb73378
CJ <tsuijing@foxmail.com>
2019-04-21 06:02:11 +0800

    wx

---
+++ onlineweb/wx/wx.py
@@ -0,0 +1,8 @@
+﻿with open('已购.txt','r',encoding='utf8') as f:
+    lines = f.readlines()
+    for line in lines:
+        with open(line.strip(),'w',encoding='utf8') as f1:
+            f1.write('')
+
+    
+    
